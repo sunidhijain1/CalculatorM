1ef2e37f51b8918f40933014033bb5c207cb28ad
Sunidhi Jain (Digital) <SU20090137@wipro.com>
2020-01-09 18:29:05 +0530

    final changes done

--- Calculator.java
+++ Calculator.java
@@ -4,11 +4,10 @@ class Calculator{
 
 public static void main(String args[]){
 	
-	String st;
-	do
-	{
 	  try {	
 	Scanner s = new Scanner(System.in);
+
+	while(true){
 
 	System.out.println("Enter the operation you want to perform");
 	System.out.println("1-Addition");
@@ -17,71 +16,56 @@ public static void main(String args[]){
 	System.out.println("4-Divison");
 	System.out.println("5-Exit");
 	int choice=s.nextInt();
+	
+	if(choice==5)
+        { 
+		System.out.println("Exiting the program...");
+		System.exit(0);
+	}
+       else {
+			System.out.println("Enter first number: ");
+			int num1=s.nextInt();
+			System.out.println("Enter second number: ");
+			int num2=s.nextInt();
 
 	int result;
 	switch(choice)
 	{
 		case 1:
-			System.out.println("Enter first number: ");
-			int num1=s.nextInt();
-			System.out.println("Enter second number: ");
-			int num2=s.nextInt();
 			result=num1+num2;
 			System.out.println("Addition is: "+result);
 			break;
 
 		case 2:
-			System.out.println("Enter first number: ");
-			int num3=s.nextInt();
-			System.out.println("Enter second number: ");
-			int num4=s.nextInt();
-			result=num3-num4;
+			result=num1-num2;
 			System.out.println("Subtraction is: "+result);
 			break;
 			
 		case 3: 
-			System.out.println("Enter first number: ");
-			int num5=s.nextInt();
-			System.out.println("Enter second number: ");
-			int num6=s.nextInt();
-			result=num5*num6;
+			result=num1*num2;
 			System.out.println("Multiplication is: "+result);
 			break; 
 
 		case 4: try{
-			System.out.println("Enter first number: ");
-			int num7=s.nextInt();
-			System.out.println("Enter second number: ");
-			int num8=s.nextInt();
-			result=num7/num8;
+			result=num1/num2;
 			System.out.println("Division is: "+result);
 			  }
 			catch (ArithmeticException e)
 			{ 
-			  System.out.println("You can't divide a number by zero, please type a valid number"); 
+			  System.out.println("divide by zero exception"); 
 			}
 			break;	
-
-		case 5:
-			System.out.println("Exiting the program...");
-			System.exit(0);
-			break;
 
 		default:
 			System.out.println("Invalid choice");
 		}
-		
+	     } 	
 	  }
-		catch (Exception e)
+	}	catch (Exception e)
 		{
-			System.out.println("Wrong input, type a numeric input");
+			System.out.println("Wrong input");
 		}
 
-		Scanner sb = new Scanner(System.in);
-		System.out.println("Do you want to continue (Press y for Yes and n for No)");
-		st=sb.next();
-		
-	}
-		while(st.equals("y") || st.equals("Y"));	
+			
  }
 }
